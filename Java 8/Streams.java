07911fc2a6a5b7440ada94d91ed9cbde595a6c13
STRO09 <sagarjanjoted123@gmail.com>
2025-07-25 07:01:01 +0530

    Streams Api and Optional done

---
+++ Java 8/Streams.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.*;
+import java.util.Optional;
+
+
+class User {
+int id;
+String name;
+public User(int id, String name) {
+
+this.id=id;
+this.name = name;
+}
+
+@Override 
+public String toString() {
+
+return "User: [ id="+this.id+", name="+this.name+" ]";
+}
+}
+
+
+class Streams {
+
+
+public static void main(String[] args) {
+
+//From a list of integers, filter even number square them collect into a new list
+
+List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+List<Integer> evensquares = nums.stream().filter(n-> n%2==0).map(n->n*n).collect(Collectors.toList());
+
+evensquares.forEach(System.out::println);
+//  From a list of names, trim spaces convert to uppercase keep only names starting with V print them
+List<String> names = Arrays.asList("Sagar","Stro","Sai","Shino","Vedika");
+
+names.stream().map(String::trim).map(n->n.toUpperCase()).filter(n->n.startsWith("V")).forEach(System.out::println);
+
+//From a list of User objects (id, name), filter users with id > 1, collect only their names into a List
+
+List<User> userlist = Arrays.asList(new User(1,"Sagar"), new User(2,"Stro"),new User(3,"Murgi"));
+
+List<User> newuserlist =  userlist.stream().filter(n->n.id>1).collect(Collectors.toList());
+
+newuserlist.forEach(System.out::println);
+
+// From a list of words, find the first word with length > 4 print it using Optional.ifPresent()
+
+List<String> words = Arrays.asList("Pen", "Rock","Paper","Scissor");
+Optional<String> _4letterwords =words.stream().filter(n->n.length()>4).findFirst();
+_4letterwords.ifPresent(System.out::println); 
+
+}}
