3c10d7cdfb24c5a632fbeb6d914ad3758299646c
STRO09 <sagarjanjoted123@gmail.com>
2025-07-28 22:17:40 +0530

    learning hibernate and organizing intellij eclipse metadata configs

---
+++ Hibernate/Hibernatedemo/src/main/java/com/study/hibernate/Hibernatedemo/App.java
@@ -0,0 +1,13 @@
+package com.study.hibernate.Hibernatedemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
